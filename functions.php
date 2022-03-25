b2952694f7c151736a96b3b565979f74eb1a851d
яαvoroηα <h.rakotonjanahary@gmail.com>
2022-03-25 17:03:03 +0300

    feat: import helpers

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ try {
 |
 */
 
-collect(['setup', 'filters'])
+collect(['setup', 'filters', 'helpers', 'medias'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
