50c72dd600a86af355281b975e78037c066cf3ee
яαvoroηα <h.rakotonjanahary@gmail.com>
2024-04-03 18:20:55 +0200

    chore: Fix phpcs erros

--- app/View/Composers/Comments.php
+++ app/View/Composers/Comments.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Copyright (c) 2024 яαvoroηα
  *
@@ -61,8 +62,18 @@ class Comments extends Composer
     {
         return sprintf(
             /* translators: %1$s is replaced with the number of comments and %2$s with the post title */
-            _nx('%1$s response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sage'),
-            get_comments_number() === 1 ? _x('One', 'comments title', 'sage') : number_format_i18n(get_comments_number()),
+            _nx(
+                '%1$s response to &ldquo;%2$s&rdquo;',
+                '%1$s responses to &ldquo;%2$s&rdquo;',
+                get_comments_number(),
+                'comments title',
+                'sage'
+            ),
+            get_comments_number() === 1 ? _x(
+                'One',
+                'comments title',
+                'sage'
+            ) : number_format_i18n(get_comments_number()),
             get_the_title()
         );
     }
